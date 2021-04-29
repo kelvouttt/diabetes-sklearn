49a2c60e9f0ad43f8474f5dc4a09cd5f26a94586
Kelvin <kelvin0898@live.com>
2021-04-29 14:02:16 +1000

    Changing the model again, this time is using the return_X_y=True parameter; adding some comments

--- test.py
+++ test.py
@@ -4,11 +4,17 @@ import numpy as np
 from sklearn import datasets, linear_model
 from sklearn.model_selection import train_test_split
 
-dataset = datasets.load_diabetes()
+# Loading the dataset from sklearn toy dataset.
+# I am using the argument return_X_y to change the Bunch object to a (
+X, y = datasets.load_diabetes(return_X_y=True)
+X = X[:, np.newaxis, 2]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.8, random_state=1)
+regressor = linear_model.LinearRegression()
+regressor.fit(X_train, y_train)
 
-X = pd.DataFrame(dataset.data, columns=dataset.feature_names)
-X['target'] = pd.Series(dataset.target)
-y = X['target']
+
+
+
 
 
 
